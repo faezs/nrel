86c985be5417743f092222da52a09aeefca0f3d8
Faez Shakil <faez.shakil@gmail.com>
2024-11-09 01:42:55 +0500

    mostly building but linker fuckery

--- flake.nix
+++ flake.nix
@@ -1,189 +1,244 @@
 {
-  description = "NREL's solarPILOT software";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
 
-    # Source repositories
-    lk-src = {
+    wxWidgets = {
+      url = "github:wxWidgets/wxWidgets";
+      flake = false;
+    };
+    lk = {
       url = "github:NREL/lk";
       flake = false;
     };
-    wex-src = {
+    wex = {
       url = "github:NREL/wex";
       flake = false;
     };
-    soltrace-src = {
-      url = "github:NREL/SolTrace";
+    googletest = {
+      url = "github:google/googletest";
       flake = false;
     };
-    solarpilot-src = {
-      url = "github:NREL/SolarPILOT";
-      flake = false;
-    };
-    ssc-src = {
+    ssc = {
       url = "github:mjwagner2/ssc/solarpilot-develop";
       flake = false;
     };
-    googletest-src = {
-      url = "github:google/googletest";
+    soltrace = {
+      url = "github:NREL/SolTrace";
+      flake = false;
+    };
+    solarpilot = {
+      url = "github:NREL/SolarPILOT";
       flake = false;
     };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ nixpkgs, systems, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" "x86_64-darwin" ];
+      imports = [ ];
+      systems = import systems;
+
       perSystem = { config, self', inputs', pkgs, system, ... }:
-      let
-        # Build wxWidgets with specific flags
-        wxGTK-static = pkgs.wxGTK32;
-
-        # Common build inputs
-        commonBuildInputs = with pkgs; [
-          wxGTK-static
-          curl.dev
-          openssl
-          nlopt
-          lp_solve
-          fontconfig
-          libGL
-          freeglut
-          gtk3
-          mesa
-          xorg.libX11
-          xorg.libXxf86vm
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXinerama
-        ];
-
-        # Function to setup source tree
-        setupSourceTree = pkgs.writeShellScriptBin "setup-source-tree" ''
-          # Create working directory structure
-          mkdir -p spt_dev/{lk,wex,ssc,SolTrace,SolarPILOT,googletest}
-
-          # Copy sources to correct locations
-          cp -r ${inputs.lk-src}/* spt_dev/lk/
-          cp -r ${inputs.wex-src}/* spt_dev/wex/
-          cp -r ${inputs.ssc-src}/* spt_dev/ssc/
-          cp -r ${inputs.soltrace-src}/* spt_dev/SolTrace/
-          cp -r ${inputs.solarpilot-src}/* spt_dev/SolarPILOT/
-          cp -r ${inputs.googletest-src}/* spt_dev/googletest/
-
-          # Create umbrella CMakeLists.txt
-          cat > spt_dev/CMakeLists.txt << 'EOF'
-          option(SAM_SKIP_TOOLS "Skips the sdktool and tcsconsole builds" ON)
-          cmake_minimum_required(VERSION 3.12)
-
-          Project(solarpilot_ui)
-          #Project(solarpilot_api)
-
-          if($${CMAKE_PROJECT_NAME} STREQUAL "solarpilot_api")
-                  option(COPILOT_API "Builds library for Copilot API" ON)
-          endif()
-
-          if($${CMAKE_PROJECT_NAME} STREQUAL "solarpilot_ui")
-                  add_subdirectory(lk)
-                  add_subdirectory(wex)
-          endif()
-
-          add_subdirectory(ssc)
-          add_subdirectory(SolTrace/coretrace)
-
-          if($${CMAKE_PROJECT_NAME} STREQUAL "solarpilot_ui")
-                  add_subdirectory(SolarPILOT)
-          endif()
-          EOF
-
-          # Make files writable
-          chmod -R u+w spt_dev
-
-          # # Build googletest
-          # cd spt_dev/googletest
-          # mkdir -p build
-          # cd build
-          # cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11
-          # make -j$NIX_BUILD_CORES
-        '';
-
-      in {
-        packages = {
-          setup-tree = setupSourceTree;
-
-          default = pkgs.stdenv.mkDerivation {
-            pname = "solarpilot";
-            version = "1.0.0";
-
-            # Create combined source
-            src = pkgs.runCommand "solarpilot-combined-src" {} ''
-              ${setupSourceTree}/bin/setup-source-tree
-              cp -r spt_dev $out
-            '';
-
-            nativeBuildInputs = with pkgs; [
-              cmake
-              pkg-config
-              gtk2
-              nlopt
-              gcc
-              gpp
-              git
-              libcurl14-openssl
-              build-essential
-
-            ];
-
-            buildInputs = commonBuildInputs;
-
-            cmakeFlags = [
-              "-DCMAKE_BUILD_TYPE=Release"
-              "-DwxWidgets_CONFIG_EXECUTABLE=${wxGTK-static}/bin/wx-config"
-              "-DGTK2_LIBRARIES=${pkgs.gtk3}/lib"
-            ];
-
-            installPhase = ''
-              mkdir -p $out/bin
-              cp SolarPILOT/deploy/aarch64/SolarPILOT $out/bin/
-            '';
-          };
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = [ self'.packages.setup-tree ];
-
-          nativeBuildInputs = with pkgs; [
+        let
+          commonBuildInputs = with pkgs; [
+            wxGTK31
+            gtk3
+            mesa
+            freeglut
+            libGLU
+            curl
+            nlopt
+            lp_solve
             cmake
             pkg-config
-            gcc
-            gdb
-            ninja
-            gtk3
+            rapidjson
+            curl
+            pkgs.libGL
           ];
-
-          buildInputs = commonBuildInputs;
-
-          shellHook = ''
-            export WXMSW3="${wxGTK-static}"
-            export WX_CONFIG="${wxGTK-static}/bin/wx-config"
-            export GTK3_LIBRARIES=${pkgs.gtk3}/lib
-            export wxWidgets_LIBRARIES=${wxGTK-static}/lib
-            export wxWidgets_INCLUDE_DIRS=${wxGTK-static}/include
-
-            # Create development directory structure if it doesn't exist
-            if [ ! -d "spt_dev" ]; then
-              setup-source-tree
+          commonInstallPhase = p: ''
+            mkdir -p $out/lib $out/include
+            cp ${p}.a $out/lib/lib${p}.a
+            cp -r ../include/* $out/include/
+            if [ -f ${p}_sandbox ]; then
+              mkdir -p $out/bin
+              cp ${p}_sandbox $out/bin/
             fi
-
-            # Set up environment variables as expected by build
-            export GTEST="$PWD/spt_dev/googletest/googletest"
-            export LKDIR="$PWD/spt_dev/lk"
-            export WEXDIR="$PWD/spt_dev/wex"
-            export SSCDIR="$PWD/spt_dev/ssc"
-            export CORETRACEDIR="$PWD/spt_dev/SolTrace/coretrace"
           '';
+          setWX = ''
+            export WXMSW3=${pkgs.wxGTK31}
+          '';
+          commonCMakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+            "-DwxWidgets_CONFIG_EXECUTABLE=${pkgs.wxGTK31}/bin/wx-config"
+            "-DwxWidgets_ROOT_DIR=${pkgs.wxGTK31}"
+            "-DCMAKE_INSTALL_LIBDIR=lib"
+          ];
+        in {
+          packages = {
+            default = self'.packages.solarpilot;
+
+            lk = pkgs.stdenv.mkDerivation {
+              pname = "lk";
+              version = "1.0.0";
+              src = inputs.lk;
+
+              nativeBuildInputs = [ pkgs.cmake pkgs.gcc pkgs.pkg-config ];
+              buildInputs = commonBuildInputs;
+
+              cmakeFlags = commonCMakeFlags;
+
+              preConfigure = setWX;
+
+              installPhase = commonInstallPhase "lk";
+            };
+
+            wex = pkgs.stdenv.mkDerivation {
+              pname = "wex";
+              version = "1.0.0";
+              src = inputs.wex;
+
+              nativeBuildInputs = [ pkgs.cmake pkgs.gcc pkgs.pkg-config ];
+              buildInputs = commonBuildInputs
+                ++ [ self'.packages.lk ];
+
+              postPatch = ''
+                rm -rf build_resources/libcurl_ssl_x64
+                substituteInPlace src/easycurl.cpp \
+                  --replace '../build_resources/libcurl_ssl_x64/include/curl/curl.h' \
+                            'curl/curl.h'
+              '';
+
+              preConfigure = ''
+                export WXMSW3=${pkgs.wxGTK31}
+                export RAPIDJSONDIR=${pkgs.rapidjson}/include
+                export CURL_DIR=${pkgs.curl.dev}
+              '';
+
+              cmakeFlags = commonCMakeFlags ++ [
+                "-DLKDIR=${self'.packages.lk}"
+                "-DSAM_SKIP_TOOLS=ON"
+                "-DUSE_SYSTEM_CURL=ON"
+              ];
+
+              installPhase = commonInstallPhase "wex";
+            };
+
+            googletest = pkgs.stdenv.mkDerivation {
+              pname = "googletest";
+              version = "1.0.0";
+              src = inputs.googletest;
+
+              nativeBuildInputs = [ pkgs.cmake pkgs.gcc ];
+
+              cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+            };
+
+            soltrace = pkgs.stdenv.mkDerivation {
+              pname = "soltrace";
+              version = "1.0.0";
+              src = inputs.soltrace;
+
+              nativeBuildInputs = [ pkgs.cmake pkgs.gcc ];
+              buildInputs = commonBuildInputs ++ [
+                self'.packages.lk
+                self'.packages.wex
+                self'.packages.googletest
+              ];
+
+              CXXFLAGS = "-Wno-deprecated";
+
+              patches = [
+                (pkgs.writeText "add-limits.patch" ''
+                  diff --git a/coretrace/treemesh.cpp b/coretrace/treemesh.cpp
+                  index xxxxxxx..xxxxxxx 100644
+                  --- a/coretrace/treemesh.cpp
+                  +++ b/coretrace/treemesh.cpp
+                  @@ -53,6 +53,7 @@
+                   #include <algorithm>
+                  +#include <limits>
+
+                   #include <unordered_map>
+                   #include <set>
+                '')
+              ];
+
+              cmakeFlags = commonCMakeFlags ++ [
+                "-DLKDIR=${self'.packages.lk}"
+                "-DWEXDIR=${self'.packages.wex}"
+                "-DGTEST=${self'.packages.googletest}"
+                # "-DCMAKE_EXE_LINKER_FLAGS=-L${self'.packages.lk}/lib -L${self'.packages.wex}/lib"
+              ];
+
+              preConfigure = "export RAPIDJSONDIR=${pkgs.rapidjson}/include";
+
+              installPhase = commonInstallPhase "soltrace";
+            };
+
+            ssc = pkgs.stdenv.mkDerivation {
+              pname = "ssc";
+              version = "1.0.0";
+              src = inputs.ssc;
+
+              nativeBuildInputs = [ pkgs.cmake pkgs.gcc ];
+              buildInputs = commonBuildInputs ++ [
+                self'.packages.lk
+                self'.packages.wex
+                self'.packages.googletest
+                self'.packages.soltrace
+              ];
+
+              preConfigure = ''
+                cd nlopt
+                ./configure
+                make
+                cd ..
+              '';
+
+              cmakeFlags = [
+                "-DCMAKE_BUILD_TYPE=Release"
+                "-DwxWidgets_CONFIG_EXECUTABLE=${pkgs.wxGTK31}/bin/wx-config"
+                "-DLKDIR=${self'.packages.lk}"
+                "-DWEXDIR=${self'.packages.wex}"
+                "-DGTEST=${self'.packages.googletest}"
+                "-DCORETRACEDIR=${self'.packages.soltrace}/coretrace"
+              ];
+            };
+
+            solarpilot = pkgs.stdenv.mkDerivation {
+              pname = "solarpilot";
+              version = "1.0.0";
+              src = inputs.solarpilot;
+
+              nativeBuildInputs = [ pkgs.cmake pkgs.gcc pkgs.makeWrapper ];
+              buildInputs = commonBuildInputs ++ [
+                self'.packages.lk
+                self'.packages.wex
+                self'.packages.googletest
+                self'.packages.soltrace
+                self'.packages.ssc
+              ];
+
+              cmakeFlags = [
+                "-DCMAKE_BUILD_TYPE=Release"
+                "-DwxWidgets_CONFIG_EXECUTABLE=${pkgs.wxGTK31}/bin/wx-config"
+                "-DLKDIR=${self'.packages.lk}"
+                "-DWEXDIR=${self'.packages.wex}"
+                "-DGTEST=${self'.packages.googletest}"
+                "-DCORETRACEDIR=${self'.packages.soltrace}/coretrace"
+                "-DSSCDIR=${self'.packages.ssc}"
+              ];
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp deploy/x64/SolarPILOT $out/bin/
+                wrapProgram $out/bin/SolarPILOT \
+                  --prefix LD_LIBRARY_PATH : ${
+                    pkgs.lib.makeLibraryPath commonBuildInputs
+                  }
+              '';
+            };
+          };
         };
-      };
     };
 }
